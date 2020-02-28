711744c55b4166f78a4ec244d2a33020609a3665
Oliver Enciso <oliverenciso07@gmail.com>
2020-02-28 18:12:40 +0800

    ArticleType and UserRights Entities Added

--- easyfmis/Entities/MstArticleTypeEntity.cs
+++ easyfmis/Entities/MstArticleTypeEntity.cs
@@ -9,12 +9,6 @@ namespace easyfmis.Entities
     class MstArticleTypeEntity
     {
         public Int32 Id { get; set; }
-        public String PayType { get; set; }
-        public Int32 AccountId { get; set; }
-        public Boolean IsLocked { get; set; }
-        public Int32 CreatedBy { get; set; }
-        public DateTime CreatedDateTime { get; set; }
-        public Int32 UpdatedBy { get; set; }
-        public DateTime UpdatedDateTime { get; set; }
+        public String ArticleType { get; set; }
     }
 }
---
+++ easyfmis/Entities/SysUserRightsEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace easyfmis.Entities
+{
+    public class SysUserRightsEntity
+    {
+        public Boolean CanDelete { get; set; }
+        public Boolean CanAdd { get; set; }
+        public Boolean CanLock { get; set; }
+        public Boolean CanUnlock { get; set; }
+        public Boolean CanPrint { get; set; }
+        public Boolean CanPreview { get; set; }
+        public Boolean CanEdit { get; set; }
+    }
+}
