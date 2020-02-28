121ef5fdbe18703fd2f65204e553ba60daf40ab6
Oliver Enciso <oliverenciso07@gmail.com>
2020-02-28 18:13:18 +0800

    SysSettings Added - Partial

---
+++ easyfmis/Forms/Software/SysSettings/SysSettingsForm.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using System.Windows.Forms;
+
+namespace easyfmis.Forms.Software.SysSettings
+{
+    public partial class SysSettingsForm : Form
+    {
+        public SysSoftwareForm sysSoftwareForm;
+
+        public Boolean isIntegrationStarted = false;
+        public Int32 logMessageCount = 0;
+
+        public String logLocation = "";
+
+        public SysSettingsForm(SysSoftwareForm softwareForm)
+        {
+            InitializeComponent();
+            sysSoftwareForm = softwareForm;
+
+            //GetComboBoxDropDownList();
+        }
+
+        private void buttonClose_Click(object sender, EventArgs e)
+        {
+            sysSoftwareForm.RemoveTabPage();
+        }
+
+        // ==========
+        // SysCurrent 
+        // ==========
+
+        //public void GetComboBoxDropDownList()
+        //{
+        //    Controllers.SysCurrentController sysSettingsController = new Controllers.SysCurrentController();
+
+        //    var terminals = sysSettingsController.DropDownTerminalList();
+        //    if (terminals.Any())
+        //    {
+
+        //        comboBoxTerminal.DataSource = terminals;
+        //        comboBoxTerminal.ValueMember = "Id";
+        //        comboBoxTerminal.DisplayMember = "Terminal";
+        //    }
+
+        //    var periods = sysSettingsController.DropDownPeriodList();
+        //    if (periods.Any())
+        //    {
+        //        comboBoxCurrentPeriod.DataSource = periods;
+        //        comboBoxCurrentPeriod.ValueMember = "Id";
+        //        comboBoxCurrentPeriod.DisplayMember = "Period";
+        //    }
+
+        //    var customers = sysSettingsController.DropDownCustomerList();
+        //    if (customers.Any())
+        //    {
+        //        comboBoxWalkinCustomer.DataSource = customers;
+        //        comboBoxWalkinCustomer.ValueMember = "Id";
+        //        comboBoxWalkinCustomer.DisplayMember = "Customer";
+        //    }
+
+        //    var discounts = sysSettingsController.DropDownDiscountList();
+        //    if (discounts.Any())
+        //    {
+        //        comboBoxDefaultDiscount.DataSource = discounts;
+        //        comboBoxDefaultDiscount.ValueMember = "Id";
+        //        comboBoxDefaultDiscount.DisplayMember = "Discount";
+        //    }
+
+        //    var supplier = sysSettingsController.DropDownSupplierList();
+        //    if (supplier.Any())
+        //    {
+        //        comboBoxReturnSupplier.DataSource = supplier;
+        //        comboBoxReturnSupplier.ValueMember = "Id";
+        //        comboBoxReturnSupplier.DisplayMember = "Supplier";
+        //    }
+        //    getSysCurrentDetail();
+        //}
+
+        public void getSysCurrentDetail()
+        {
+            //    Controllers.SysCurrentController sysSettingsController = new Controllers.SysCurrentController();
+            //    var sysCurrent = sysSettingsController.GetSysCurrent();
+            //    if (sysCurrent != null)
+            //    {
+            //        textBoxCompanyName.Text = sysCurrent.CompanyName;
+            //        textBoxAddress.Text = sysCurrent.Address;
+            //        textBoxContactNo.Text = sysCurrent.ContactNo;
+            //        textBoxTIN.Text = sysCurrent.TIN;
+            //        textBoxAccreditationNo.Text = sysCurrent.AccreditationNo;
+            //        textBoxSerialNo.Text = sysCurrent.SerialNo;
+            //        textBoxPermitNo.Text = sysCurrent.PermitNo;
+            //        textBoxMachineNo.Text = sysCurrent.MachineNo;
+            //        textBoxReceiptFooter.Text = sysCurrent.ReceiptFooter;
+            //        textBoxInvoiceFooter.Text = sysCurrent.InvoiceFooter;
+            //        textBoxLicenseCode.Text = sysCurrent.LicenseCode;
+            //        textBoxTenantOf.Text = sysCurrent.TenantOf;
+            //        textBoxCurrentVersion.Text = sysCurrent.CurrentVersion;
+            //        textBoxCurrentDeveloper.Text = sysCurrent.CurrentDeveloper;
+            //        textBoxCurrentSupport.Text = sysCurrent.CurrentSupport;
+            //        comboBoxCurrentPeriod.SelectedValue = Convert.ToInt32(sysCurrent.CurrentPeriodId);
+            //        comboBoxTerminal.SelectedValue = Convert.ToInt32(sysCurrent.TerminalId);
+            //        comboBoxWalkinCustomer.SelectedValue = Convert.ToInt32(sysCurrent.WalkinCustomerId);
+            //        comboBoxDefaultDiscount.SelectedValue = Convert.ToInt32(sysCurrent.DefaultDiscountId);
+            //        comboBoxReturnSupplier.SelectedValue = Convert.ToInt32(sysCurrent.ReturnSupplierId);
+            //        textBoxORPrintTitle.Text = sysCurrent.ORPrintTitle;
+            //        checkBoxIsTenderPrint.Checked = Convert.ToBoolean(sysCurrent.IsTenderPrint);
+            //        checkBoxIsBarcodeQuantityAlwaysOne.Checked = Convert.ToBoolean(sysCurrent.IsBarcodeQuantityAlwaysOne);
+            //        checkBoxWithCustomerDisplay.Checked = Convert.ToBoolean(sysCurrent.WithCustomerDisplay);
+            //        textBoxCustomerDisplayPort.Text = sysCurrent.CustomerDisplayPort;
+            //        textBoxCustomerDisplayBaudRate.Text = sysCurrent.CustomerDisplayBaudRate;
+            //        textBoxCustomerDisplayFirstLineMessage.Text = sysCurrent.CustomerDisplayFirstLineMessage;
+            //        textBoxCustomerDisplayIfCounterClosedMessage.Text = sysCurrent.CustomerDisplayIfCounterClosedMessage;
+            //        textBoxCollectionReport.Text = sysCurrent.CollectionReport;
+            //        textBoxZReadingFooter.Text = sysCurrent.ZReadingFooter;
+            //        textBoxEasypayAPIURL.Text = sysCurrent.EasypayAPIURL;
+            //        textBoxEasypayDefaultUsername.Text = sysCurrent.EasypayDefaultUsername;
+            //        textBoxEasypayDefaultPassword.Text = sysCurrent.EasypayDefaultPassword;
+            //        textBoxEasypayMotherCardNumber.Text = sysCurrent.EasypayMotherCardNumber;
+            //        checkBoxActivateAuditTrail.Checked = Convert.ToBoolean(sysCurrent.ActivateAuditTrail);
+            //    }
+        }
+
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            //Controllers.SysCurrentController sysSettingsController = new Controllers.SysCurrentController();
+
+            //Entities.SysCurrentEntity sysCurrentEntity = new Entities.SysCurrentEntity()
+            //{
+            //    CompanyName = textBoxCompanyName.Text,
+            //    Address = textBoxAddress.Text,
+            //    ContactNo = textBoxContactNo.Text,
+            //    TIN = textBoxTIN.Text,
+            //    AccreditationNo = textBoxAccreditationNo.Text,
+            //    SerialNo = textBoxSerialNo.Text,
+            //    PermitNo = textBoxPermitNo.Text,
+            //    MachineNo = textBoxMachineNo.Text,
+            //    DeclareRate = "",
+            //    ReceiptFooter = textBoxInvoiceFooter.Text,
+            //    InvoiceFooter = textBoxInvoiceFooter.Text,
+            //    LicenseCode = textBoxLicenseCode.Text,
+            //    TenantOf = textBoxTenantOf.Text,
+            //    CurrentVersion = textBoxCurrentVersion.Text,
+            //    CurrentDeveloper = textBoxCurrentDeveloper.Text,
+            //    CurrentSupport = textBoxCurrentSupport.Text,
+            //    CurrentPeriodId = comboBoxCurrentPeriod.SelectedValue.ToString(),
+            //    TerminalId = comboBoxTerminal.SelectedValue.ToString(),
+            //    WalkinCustomerId = comboBoxWalkinCustomer.SelectedValue.ToString(),
+            //    DefaultDiscountId = comboBoxDefaultDiscount.SelectedValue.ToString(),
+            //    ReturnSupplierId = comboBoxReturnSupplier.SelectedValue.ToString(),
+            //    ORPrintTitle = textBoxORPrintTitle.Text,
+            //    IsTenderPrint = checkBoxIsTenderPrint.Checked.ToString(),
+            //    IsBarcodeQuantityAlwaysOne = checkBoxIsBarcodeQuantityAlwaysOne.Checked.ToString(),
+            //    WithCustomerDisplay = checkBoxWithCustomerDisplay.Checked.ToString(),
+            //    CustomerDisplayPort = textBoxCustomerDisplayPort.Text,
+            //    CustomerDisplayBaudRate = textBoxCustomerDisplayBaudRate.Text,
+            //    CustomerDisplayFirstLineMessage = textBoxCustomerDisplayFirstLineMessage.Text,
+            //    CustomerDisplayIfCounterClosedMessage = textBoxCustomerDisplayIfCounterClosedMessage.Text,
+            //    CollectionReport = textBoxCollectionReport.Text,
+            //    ZReadingFooter = textBoxZReadingFooter.Text,
+            //    EasypayAPIURL = textBoxEasypayAPIURL.Text,
+            //    EasypayDefaultUsername = textBoxEasypayDefaultUsername.Text,
+            //    EasypayDefaultPassword = textBoxEasypayDefaultPassword.Text,
+            //    EasypayMotherCardNumber = textBoxEasypayMotherCardNumber.Text,
+            //    ActivateAuditTrail = checkBoxActivateAuditTrail.Checked.ToString()
+            //};
+
+            //String[] saveSysCurrent = sysSettingsController.UpdateSysCurrent(sysCurrentEntity);
+            //if (saveSysCurrent[1].Equals("0") == false)
+            //{
+            //    MessageBox.Show("SysCurrent Save", "Easy POS", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            //}
+            //else
+            //{
+            //    MessageBox.Show(saveSysCurrent[0], "Easy POS", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            //}
+        }
+
+        private void tabControl1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            //if (tabControl1.SelectedTab.Name == "System Current") { textBoxCompanyName.Focus(); }
+            //if (tabControl1.SelectedTab.Name == "Cloud Settings") { dtpIntegrationDate.Focus(); }
+
+        }
+    }
+}
---
+++ easyfmis/Forms/Software/SysSettings/SysSettingsForm.designer.cs
@@ -0,0 +1,230 @@
+﻿namespace easyfmis.Forms.Software.SysSettings
+{
+    partial class SysSettingsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SysSettingsForm));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.backgroundWorkerEasyfisIntegration = new System.ComponentModel.BackgroundWorker();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.buttonSave);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.buttonClose);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1400, 63);
+            this.panel1.TabIndex = 5;
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonSave.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(166)))), ((int)(((byte)(240)))));
+            this.buttonSave.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(166)))), ((int)(((byte)(240)))));
+            this.buttonSave.FlatAppearance.BorderSize = 0;
+            this.buttonSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonSave.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSave.ForeColor = System.Drawing.Color.White;
+            this.buttonSave.Location = new System.Drawing.Point(1206, 11);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(88, 40);
+            this.buttonSave.TabIndex = 7;
+            this.buttonSave.TabStop = false;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = false;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::easyfmis.Properties.Resources.Settings;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(48, 40);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(63, 17);
+            this.label1.Margin = new System.Windows.Forms.Padding(0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 35);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Settings";
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonClose.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(79)))), ((int)(((byte)(28)))));
+            this.buttonClose.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(79)))), ((int)(((byte)(28)))));
+            this.buttonClose.FlatAppearance.BorderSize = 0;
+            this.buttonClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonClose.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonClose.ForeColor = System.Drawing.Color.White;
+            this.buttonClose.Location = new System.Drawing.Point(1300, 12);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(88, 40);
+            this.buttonClose.TabIndex = 1;
+            this.buttonClose.Text = "Close";
+            this.buttonClose.UseVisualStyleBackColor = false;
+            this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.tabControl1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 63);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1400, 637);
+            this.panel2.TabIndex = 6;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1400, 637);
+            this.tabControl1.TabIndex = 35;
+            this.tabControl1.SelectedIndexChanged += new System.EventHandler(this.tabControl1_SelectedIndexChanged);
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.panel4);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.pictureBox3);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1392, 608);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "System Settings";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // panel4
+            // 
+            this.panel4.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel4.BackColor = System.Drawing.SystemColors.Control;
+            this.panel4.Location = new System.Drawing.Point(8, 49);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(1376, 556);
+            this.panel4.TabIndex = 33;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold);
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label3.Location = new System.Drawing.Point(69, 10);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(233, 28);
+            this.label3.TabIndex = 31;
+            this.label3.Text = "Easyerp System Settings";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::easyfmis.Properties.Resources.easyerp;
+            this.pictureBox3.Location = new System.Drawing.Point(8, 6);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(55, 37);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 32;
+            this.pictureBox3.TabStop = false;
+            // 
+            // backgroundWorkerEasyfisIntegration
+            // 
+            this.backgroundWorkerEasyfisIntegration.WorkerReportsProgress = true;
+            this.backgroundWorkerEasyfisIntegration.WorkerSupportsCancellation = true;
+            // 
+            // SysSettingsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(1400, 700);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "SysSettingsForm";
+            this.Text = "Settings";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonClose;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.ComponentModel.BackgroundWorker backgroundWorkerEasyfisIntegration;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button buttonSave;
+    }
+}
