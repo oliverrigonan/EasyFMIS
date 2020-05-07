350bca9eef681a19bca5badbb0b81080567ff659
Oliver Enciso <oliverenciso07@gmail.com>
2020-05-07 15:02:46 +0800

    SalesInvoice - Discount Amount Added TrnSalesInvoiceDetailDiscountForm

--- easyfmis/Forms/Software/TrnSalesInvoice/TrnSalesInvoiceDetailDiscountForm.cs
+++ easyfmis/Forms/Software/TrnSalesInvoice/TrnSalesInvoiceDetailDiscountForm.cs
@@ -14,6 +14,7 @@ namespace easyfmis.Forms.Software.TrnSalesInvoice
     {
         public TrnSalesInvoiceDetailForm trnSalesInvoiceDetailForm;
         public Entities.TrnSalesInvoiceEntity trnSalesInvoiceEntity;
+        public Decimal totalSIPrice = 0;
 
         public TrnSalesInvoiceDetailDiscountForm(TrnSalesInvoiceDetailForm salesInvoiceDetailForm, Entities.TrnSalesInvoiceEntity salesInvoiceEntity)
         {
@@ -21,6 +22,8 @@ namespace easyfmis.Forms.Software.TrnSalesInvoice
 
             trnSalesInvoiceDetailForm = salesInvoiceDetailForm;
             trnSalesInvoiceEntity = salesInvoiceEntity;
+            totalSIPrice = salesInvoiceDetailForm.SITotalItemPrice;
+
 
             GetDiscount();
         }
@@ -43,39 +46,28 @@ namespace easyfmis.Forms.Software.TrnSalesInvoice
                 return;
             }
 
-            if (comboBoxDiscount.Text.ToLower().Contains("variable")) {
+            if (comboBoxDiscount.Text.ToLower().Contains("variable"))
+            {
                 textBoxDiscountRate.ReadOnly = false;
+                textBoxDiscountAmount.ReadOnly = false;
             }
 
             var selectedItem = (Entities.MstDiscountEntity)comboBoxDiscount.SelectedItem;
             if (selectedItem != null)
             {
                 textBoxDiscountRate.Text = selectedItem.DiscountRate.ToString("#,##0.00");
+                ComputeDiscountAmount();
             }
         }
 
         public void GetSalesDiscountInformation()
         {
-            //Controllers.TrnSalesController trnSalesController = new Controllers.TrnSalesController();
-
             Int32? discountId = null;
-            String seniorCitizenID = "";
-
-            //if (trnSalesInvoiceDetailForm != null)
-            //{
-            //    discountId = trnSalesController.DiscountDetailSales(trnSalesInvoiceDetailForm.trnSalesEntity.Id).DiscountId;
-            //    seniorCitizenID = trnSalesController.DiscountDetailSales(trnSalesInvoiceDetailForm.trnSalesEntity.Id).SeniorCitizenId;
-            //    seniorCitizenName = trnSalesController.DiscountDetailSales(trnSalesInvoiceDetailForm.trnSalesEntity.Id).SeniorCitizenName;
-            //    seniorCitizenAge = trnSalesController.DiscountDetailSales(trnSalesInvoiceDetailForm.trnSalesEntity.Id).SeniorCitizenAge.ToString();
-            //}
-
             if (discountId != null)
             {
                 comboBoxDiscount.SelectedValue = discountId;
-                textBoxDiscountRate.Text = seniorCitizenID;
-               
             }
-            
+
         }
 
         private void buttonSave_Click(object sender, EventArgs e)
@@ -122,7 +114,12 @@ namespace easyfmis.Forms.Software.TrnSalesInvoice
         {
             if (String.IsNullOrEmpty(textBoxDiscountRate.Text))
             {
-                textBoxDiscountRate.Text = Convert.ToDecimal(0).ToString("#,##0.00###");
+                textBoxDiscountRate.Text = Convert.ToDecimal(0).ToString("#,##0.00"); ;
+            }
+
+            if (String.IsNullOrEmpty(textBoxDiscountAmount.Text))
+            {
+                textBoxDiscountAmount.Text = Convert.ToDecimal(0).ToString("#,##0.00");
             }
         }
 
@@ -144,9 +141,96 @@ namespace easyfmis.Forms.Software.TrnSalesInvoice
             }
         }
 
+        private void textBoxDiscountAmount_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '.') && (e.KeyChar != '-'))
+            {
+                e.Handled = true;
+            }
+
+            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
+
+            if ((e.KeyChar == '-') && ((sender as TextBox).Text.IndexOf('-') > -1))
+            {
+                e.Handled = true;
+            }
+        }
+
         private void textBoxDiscountRate_Leave(object sender, EventArgs e)
         {
             DefaultZeroEmptyString();
+        }
+
+        private void textBoxDiscountAmount_Leave(object sender, EventArgs e)
+        {
+            DefaultZeroEmptyString();
+        }
+
+        private void textBoxDiscountRate_TextChanged(object sender, EventArgs e)
+        {
+            ComputeDiscountAmount();
+        }
+
+        private void textBoxDiscountAmount_TextChanged(object sender, EventArgs e)
+        {
+            ComputeDiscountRate();
+        }
+
+        public void ComputeDiscountRate()
+        {
+            try
+            {
+                Decimal price = totalSIPrice;
+
+                Decimal discountAmount = Convert.ToDecimal(textBoxDiscountAmount.Text);
+                Decimal discountRate = 0;
+
+                if (discountAmount > 0)
+                {
+                    discountRate = (discountAmount / price) * 100;
+                }
+                else
+                {
+                    discountRate = 0;
+                }
+
+                textBoxDiscountRate.Text = discountRate.ToString("#,##0.00");
+                //textBoxDiscountRate.Text = discountRate.ToString("#,##0.00###");
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "Easy POS", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public void ComputeDiscountAmount()
+        {
+            try
+            {
+                Decimal price = totalSIPrice;
+
+                Decimal discountAmount = 0;
+                Decimal discountRate = Convert.ToDecimal(textBoxDiscountRate.Text);
+
+                if (discountRate > 0)
+                {
+                    discountAmount = (totalSIPrice * discountRate) / 100;
+                }
+                else
+                {
+                    discountAmount = 0;
+                }
+
+                textBoxDiscountAmount.Text = discountAmount.ToString("#,##0.00");
+                //textBoxDiscountAmount.Text = discountAmount.ToString("#,##0.00###");
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "Easy POS", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
--- easyfmis/Forms/Software/TrnSalesInvoice/TrnSalesInvoiceDetailDiscountForm.designer.cs
+++ easyfmis/Forms/Software/TrnSalesInvoice/TrnSalesInvoiceDetailDiscountForm.designer.cs
@@ -35,6 +35,8 @@
             this.buttonClose = new System.Windows.Forms.Button();
             this.buttonSave = new System.Windows.Forms.Button();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.textBoxDiscountAmount = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.comboBoxDiscount = new System.Windows.Forms.ComboBox();
             this.textBoxDiscountRate = new System.Windows.Forms.TextBox();
@@ -55,7 +57,7 @@
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Margin = new System.Windows.Forms.Padding(2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(471, 50);
+            this.panel1.Size = new System.Drawing.Size(485, 50);
             this.panel1.TabIndex = 4;
             // 
             // pictureBox1
@@ -89,7 +91,7 @@
             this.buttonClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonClose.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.buttonClose.ForeColor = System.Drawing.Color.White;
-            this.buttonClose.Location = new System.Drawing.Point(391, 10);
+            this.buttonClose.Location = new System.Drawing.Point(405, 10);
             this.buttonClose.Margin = new System.Windows.Forms.Padding(2);
             this.buttonClose.Name = "buttonClose";
             this.buttonClose.Size = new System.Drawing.Size(70, 32);
@@ -108,7 +110,7 @@
             this.buttonSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonSave.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.buttonSave.ForeColor = System.Drawing.Color.White;
-            this.buttonSave.Location = new System.Drawing.Point(316, 10);
+            this.buttonSave.Location = new System.Drawing.Point(330, 10);
             this.buttonSave.Margin = new System.Windows.Forms.Padding(2);
             this.buttonSave.Name = "buttonSave";
             this.buttonSave.Size = new System.Drawing.Size(70, 32);
@@ -120,6 +122,8 @@
             // 
             // panel2
             // 
+            this.panel2.Controls.Add(this.textBoxDiscountAmount);
+            this.panel2.Controls.Add(this.label4);
             this.panel2.Controls.Add(this.label3);
             this.panel2.Controls.Add(this.comboBoxDiscount);
             this.panel2.Controls.Add(this.textBoxDiscountRate);
@@ -128,14 +132,39 @@
             this.panel2.Location = new System.Drawing.Point(0, 50);
             this.panel2.Margin = new System.Windows.Forms.Padding(2);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(471, 75);
+            this.panel2.Size = new System.Drawing.Size(485, 105);
             this.panel2.TabIndex = 5;
+            // 
+            // textBoxDiscountAmount
+            // 
+            this.textBoxDiscountAmount.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.textBoxDiscountAmount.Location = new System.Drawing.Point(146, 70);
+            this.textBoxDiscountAmount.Margin = new System.Windows.Forms.Padding(2);
+            this.textBoxDiscountAmount.Name = "textBoxDiscountAmount";
+            this.textBoxDiscountAmount.ReadOnly = true;
+            this.textBoxDiscountAmount.Size = new System.Drawing.Size(330, 29);
+            this.textBoxDiscountAmount.TabIndex = 38;
+            this.textBoxDiscountAmount.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.textBoxDiscountAmount.TextChanged += new System.EventHandler(this.textBoxDiscountAmount_TextChanged);
+            this.textBoxDiscountAmount.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxDiscountAmount_KeyPress);
+            this.textBoxDiscountAmount.Leave += new System.EventHandler(this.textBoxDiscountAmount_Leave);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.label4.Location = new System.Drawing.Point(8, 73);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(134, 21);
+            this.label4.TabIndex = 39;
+            this.label4.Text = "Discount Amount:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Segoe UI", 12F);
-            this.label3.Location = new System.Drawing.Point(54, 7);
+            this.label3.Location = new System.Drawing.Point(68, 7);
             this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(74, 21);
@@ -146,7 +175,7 @@
             // 
             this.comboBoxDiscount.Font = new System.Drawing.Font("Segoe UI", 12F);
             this.comboBoxDiscount.FormattingEnabled = true;
-            this.comboBoxDiscount.Location = new System.Drawing.Point(133, 5);
+            this.comboBoxDiscount.Location = new System.Drawing.Point(146, 5);
             this.comboBoxDiscount.Margin = new System.Windows.Forms.Padding(2);
             this.comboBoxDiscount.Name = "comboBoxDiscount";
             this.comboBoxDiscount.Size = new System.Drawing.Size(330, 29);
@@ -156,13 +185,14 @@
             // textBoxDiscountRate
             // 
             this.textBoxDiscountRate.Font = new System.Drawing.Font("Segoe UI", 12F);
-            this.textBoxDiscountRate.Location = new System.Drawing.Point(133, 38);
+            this.textBoxDiscountRate.Location = new System.Drawing.Point(146, 38);
             this.textBoxDiscountRate.Margin = new System.Windows.Forms.Padding(2);
             this.textBoxDiscountRate.Name = "textBoxDiscountRate";
             this.textBoxDiscountRate.ReadOnly = true;
             this.textBoxDiscountRate.Size = new System.Drawing.Size(330, 29);
             this.textBoxDiscountRate.TabIndex = 1;
             this.textBoxDiscountRate.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.textBoxDiscountRate.TextChanged += new System.EventHandler(this.textBoxDiscountRate_TextChanged);
             this.textBoxDiscountRate.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxDiscountRate_KeyPress);
             this.textBoxDiscountRate.Leave += new System.EventHandler(this.textBoxDiscountRate_Leave);
             // 
@@ -170,7 +200,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI", 12F);
-            this.label2.Location = new System.Drawing.Point(20, 41);
+            this.label2.Location = new System.Drawing.Point(33, 41);
             this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(109, 21);
@@ -181,7 +211,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
-            this.ClientSize = new System.Drawing.Size(471, 125);
+            this.ClientSize = new System.Drawing.Size(485, 155);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Font = new System.Drawing.Font("Segoe UI", 10.2F);
@@ -213,5 +243,7 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.ComboBox comboBoxDiscount;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxDiscountAmount;
+        private System.Windows.Forms.Label label4;
     }
 }
